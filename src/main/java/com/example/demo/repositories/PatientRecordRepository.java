2bd3591de6993c4076088ae8350d53e177152811
NISHANT MIYANI <88224248+Nishant1782@users.noreply.github.com>
2022-09-12 13:13:40 +0530

    Update PatientRecordRepository.java

--- src/main/java/com/example/demo/repositories/PatientRecordRepository.java
+++ src/main/java/com/example/demo/repositories/PatientRecordRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repositories;
 import com.example.demo.model.PatientRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
 
 }
