cf406d5fcb17314339f5d86d1e91232a55a11e66
NISHANT MIYANI <88224248+Nishant1782@users.noreply.github.com>
2022-09-19 12:45:24 +0530

    Completed The Task

--- src/main/java/com/example/demo/repositories/PatientRecordRepository.java
+++ src/main/java/com/example/demo/repositories/PatientRecordRepository.java
@@ -4,6 +4,4 @@ import com.example.demo.model.PatientRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
-
-}
+public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {}
