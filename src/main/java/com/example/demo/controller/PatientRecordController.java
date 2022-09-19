4bf8b8be2666344b6b6b65ffbc3f48149790125d
NISHANT MIYANI <88224248+Nishant1782@users.noreply.github.com>
2022-09-19 12:44:05 +0530

    Completed The Task

--- src/main/java/com/example/demo/controller/PatientRecordController.java
+++ src/main/java/com/example/demo/controller/PatientRecordController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class PatientRecordController {
 
+    
     @Autowired
     PatientRecordRepository patientRecordRepository;
 
